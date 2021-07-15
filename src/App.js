f20464320f38a4de34b337f943e402d14fb22249
Jesse Clark <hello@developerjesse.com>
2021-07-15 13:30:00 +0300

    poc with getting address from the device

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import Eth from 'ethjs'
+import Web3 from 'web3'
 import './App.css';
+import LedgerConnectProvider from './LedgerConnectProvider'
+
+const ledgerConnect = new LedgerConnectProvider({
+  chainId: 31,
+  rpcUrl: 'https://public-node.testnet.rsk.co'
+})
 
 function App() {
+  const [account, setAccount] = useState('')
+  const [chainId, setChainId] = useState('')
+
+  const connectToLedger = () => {
+    console.log('connecting to ledger...')
+
+    ledgerConnect.connect().then(provider => {
+      console.log('the then...')
+      console.log('provider', provider)
+      console.log('ledger provider:', provider._providers[0])
+
+      /*
+      provider._providers[0].getAccounts(res => console.log('accounts:', res))
+      const ethQuery = new Eth(provider)
+      ethQuery.net_version().then(chainId => setChainId(chainId))
+      ethQuery.accounts().then(console.log)
+      
+      const web3 = new Web3(provider)
+      web3.eth.getAccounts().then(console.log)
+      */
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ledger POC</h1>
+      <button onClick={connectToLedger}>Connect to Ledger</button>
+      <p>accounts: {account}</p>
+      <p>chainId: {chainId}</p>
     </div>
   );
 }
---
+++ src/LedgerConnectProvider.js
@@ -0,0 +1,39 @@
+import ProviderEngine from "web3-provider-engine";
+import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
+import createLedgerSubprovider from "@ledgerhq/web3-subprovider";
+import RpcSubprovider from 'web3-provider-engine/subproviders/rpc'
+
+class LedgerProvider {
+  constructor(opts) {
+    this.chainId = opts.chainId
+    this.rpcUrl = opts.rpcUrl
+  }
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      console.log('calling connect...', this.chainId, this.rpcUrl)
+      const engine = new ProviderEngine();
+      const getTransport = () => TransportWebUSB.create();
+      const ledger = createLedgerSubprovider(getTransport, {
+          networkId: 31,
+          accountsLength: 1, // get a single account
+          // paths: ["44'/60'/x'/0/0", "44'/60'/0'/x"],
+      });
+      engine.addProvider(ledger);
+      engine.addProvider(new RpcSubprovider({ rpcUrl: 'https://public-node.testnet.rsk.co' }));
+      engine.start();
+
+      // this will prompt the window to connect:
+      ledger.getAccounts((error, response) => {
+        console.log({ error, response })
+        if (error) {
+          reject(error)
+        }
+      })
+
+      resolve(engine)
+    })
+  }
+}
+
+export default LedgerProvider
